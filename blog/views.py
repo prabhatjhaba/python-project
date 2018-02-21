8b4ad9f9c9ff6f9661733d78134b597adddbfa98
prabhat jhaba <mca.prabhat86@gmail.com>
2018-02-21 11:38:05 +0530

    add blog form

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,30 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+#from .forms import *
 # Create your views here.
 #def index(request):
 
     #return HttpResponse("Hello, world. You're at the Blog index.")
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        post = form.save(commit=False)
+        post.author = request.user
+        post.published_date = timezone.now()
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_new.html', {'form': form})
